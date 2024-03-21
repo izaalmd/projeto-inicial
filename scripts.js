34bdad8a7dcb579c9f0d222c93835303b701ca77
izaalmd <rm90089@estudante.fieb.edu.br>
2024-03-21 09:15:00 -0300

    cep

--- scripts.js
+++ scripts.js
@@ -1,6 +1,35 @@
 var botao = document.getElementById("botao");
-botao = addEventListener("click", alertar);
+botao.addEventListener("click", alertar);
+var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
 
-function alertar(){
-    alert("Voce clicou no botão!!!!");
+function alertar(event){
+    //alert("Voce clicou no botão!!!!");//
+
+    const url = `http://viacep.com.br/ws/${cep.value}/json`;
+    fetch(url)
+    .then(resposta=>resposta.json)
+    .then(dados=>alert(dados.bairro))
+
+
+    saida.innerText = "Nome: " +nome.value + 
+    "\n Email " + email.value +
+    "\n Telefone " + telefone.value +
+    "\n cep " + cep.value +
+    "\n logradouro " + logradouro.value +
+    "\n numero " + numero.value +
+    "\n complemento " + complemento.value +
+    "\n bairro " + bairro.value +
+    "\n cidade " + cidade.value +
+    "\n estado " + estado.value ;
+
 }
